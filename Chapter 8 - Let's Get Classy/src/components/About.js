137e0bf174ffeb3e51e2d17c2d80772f2a302dfa
Mikhil <mighilmurali@gmail.com>
2024-06-16 12:29:55 +0530

    episode 8

---
+++ Chapter 8 - Let's Get Classy/src/components/About.js
@@ -0,0 +1,28 @@
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    // console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    // console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    // console.log("Parent Render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>Namaste Javascript</h2>
+        <User name="Mikhil Function" />
+        {/* <UserClass name="First" location="Calicut class" /> */}
+      </div>
+    );
+  }
+}
+
+export default About;
